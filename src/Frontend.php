74511ffe3f55094a50e885dd1bfd29f2f8a410f3
vasikgreif <info@vaclavgreif.cz>
2020-05-09 10:10:16 +0200

    Add method for frontend assets

--- src/Frontend.php
+++ src/Frontend.php
@@ -17,6 +17,7 @@ class Frontend extends Component
   {
     $this->assets = $assets;
   }
+
   public function setup()
   {
     $this->register_frontend_scripts();
@@ -24,8 +25,22 @@ class Frontend extends Component
     add_action('wp_footer', [$this, 'test']);
   }
 
+  /**
+   * Add assets
+   */
   public function register_frontend_scripts()
   {
+    $this->assets->add_assets($this->get_assets());
+  }
+
+  /**
+   * Get the needed assets
+   * @return array
+   * @throws \Exception
+   */
+  public function get_assets()
+  {
+    $assets = [];
     $vendors      = $this->assets->asset('vendors~plugin.js');
     $vendors_deps = [];
 
@@ -33,34 +48,35 @@ class Frontend extends Component
     $main_deps = ['react', 'react-dom', 'wp-i18n'];
 
     if ($vendors) {
-      $this->assets->add_asset(
-        [
-          'handle' => 'wpify-vendors',
-          'file' => $vendors,
-          'deps' => $vendors_deps
-        ]
-      );
+      $assets[] = [
+        'handle' => 'wpify-vendors',
+        'file'   => $vendors,
+        'deps'   => $vendors_deps,
+      ];
+
       $main_deps[] = 'wpify-vendors';
     }
 
     if ($main) {
-      $this->assets->add_asset(
-        [
-          'handle' => 'wpify',
-          'file' => $main,
-          'deps' => $main_deps,
-          'localize' => [
-            'wpify' => [
-              'publicPath' => $this->plugin->get_asset_url('build/'),
-              'restUrl' => $this->plugin->get_api_manager()->get_rest_url(),
-              'nonce' => wp_create_nonce($this->plugin->get_api_manager()->get_nonce_action()),
-            ]
-          ]
-        ]
-      );
+      $assets[] = [
+        'handle'   => 'wpify',
+        'file'     => $main,
+        'deps'     => $main_deps,
+        'localize' => [
+          'wpify' => [
+            'publicPath' => $this->plugin->get_asset_url('build/'),
+            'restUrl'    => $this->plugin->get_api_manager()->get_rest_url(),
+            'nonce'      => wp_create_nonce($this->plugin->get_api_manager()->get_nonce_action()),
+          ],
+        ],
+      ];
     }
+    return $assets;
   }
 
+  /**
+   * Render React root div
+   */
   public function print_react_root()
   {
     echo '<div id="react-root"></div>';
