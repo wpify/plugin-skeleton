12f30bc1574d4608c4fcb428045f8101c05081f9
Daniel Mejta <daniel@mejta.net>
2020-05-12 09:43:17 +0200

    fix

--- src/Settings.php
+++ src/Settings.php
@@ -108,12 +108,12 @@ class Settings extends AbstractComponent
    */
   public function admin_page_display()
   {
-?>
+    ?>
     <div class="wrap cmb2-options-page <?php echo $this->key; ?>">
       <h2><?php echo esc_html(get_admin_page_title()); ?></h2>
       <?php cmb2_metabox_form($this->metabox_id, $this->key); ?>
     </div>
-<?php
+    <?php
   }
 
   /**
