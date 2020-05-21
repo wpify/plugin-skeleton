5db34e3acf8dc735ae56845cd7e7c0b0b4dc995c
vasikgreif <info@vaclavgreif.cz>
2020-05-21 20:02:54 +0200

    Make the Controller more abstract

--- src/Controller.php
+++ src/Controller.php
@@ -11,29 +11,6 @@ use Wpify\Core\AbstractController;
  */
 class Controller extends AbstractController
 {
-  private $view;
-  private $assets = [];
-
-  /**
-   * Set the view to render
-   *
-   * @param $view
-   */
-  public function set_view($view)
-  {
-    $this->view = $view;
-  }
-
-  /**
-   * Set assets
-   *
-   * @param $assets
-   */
-  public function set_assets($assets)
-  {
-    $this->assets = $assets;
-  }
-
   /**
    * Render the assets and view
    *
@@ -47,10 +24,10 @@ class Controller extends AbstractController
   public function render($data = [], $view = '', $return = false, $render_assets = true)
   {
     if (!$view) {
-      $view = $this->view;
+      $view = $this->get_view();
     }
-    if (!empty($this->assets) && $render_assets) {
-      $this->plugin->print_assets(...$this->assets);
+    if (!empty($this->get_assets()) && $render_assets) {
+      $this->plugin->print_assets(...$this->get_assets());
     }
     $this->plugin->get_view()->render($view, $data, $return);
   }
