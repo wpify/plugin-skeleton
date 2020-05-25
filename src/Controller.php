81f6e452bf1c392804b9f3cf49c19afd78e51282
vasikgreif <info@vaclavgreif.cz>
2020-05-25 12:28:44 +0200

    Fix

--- src/Controller.php
+++ src/Controller.php
@@ -15,20 +15,20 @@ class Controller extends AbstractController
    * Render the assets and view
    *
    * @param array $data
-   * @param string $view
+   * @param string $template
    * @param bool $return
    * @param bool $render_assets
    *
    * @throws \Exception
    */
-  public function render($data = [], $view = '', $return = false, $render_assets = true)
+  public function render($data = [], $template = '', $return = false, $render_assets = true)
   {
-    if (!$view) {
-      $view = $this->get_view();
+    if (!$template) {
+      $template = $this->get_template();
     }
     if (!empty($this->get_assets()) && $render_assets) {
       $this->plugin->print_assets(...$this->get_assets());
     }
-    $this->plugin->get_view()->render($view, $data, $return);
+    $this->plugin->get_view()->render($template, $data, $return);
   }
 }
