59796a8ce706978fd2e8af2f055f12f2f08451b2
Daniel Mejta <daniel@mejta.net>
2020-05-25 18:16:06 +0200

    tools

---
+++ src/Managers/ToolsManager.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Wpify\Managers;
+
+use Wpify\Core\AbstractManager;
+use Wpify\Cpt\BookPostType;
+use Wpify\Tools\CopyrightShortcode;
+use Wpify\Tools\CustomSMTP;
+use Wpify\Tools\DeferScripts;
+use Wpify\Tools\DisableEmbeds;
+use Wpify\Tools\DisableEmojis;
+use Wpify\Tools\DisableXmlRpc;
+use Wpify\Tools\RemoveAccentInFilenames;
+use Wpify\Tools\RemoveScriptVersion;
+
+/**
+ * Class CptManager
+ * @package Wpify\Managers
+ * @property BookPostType $MyPostType
+ */
+class CptManager extends AbstractManager
+{
+  protected $modules = [
+    CopyrightShortcode::class,
+    CustomSMTP::class,
+    DeferScripts::class,
+    DisableEmbeds::class,
+    DisableEmojis::class,
+    DisableXmlRpc::class,
+    RemoveAccentInFilenames::class,
+    RemoveScriptVersion::class,
+  ];
+}
